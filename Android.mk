275383928417d94eb1ed17fe8c2f43d8fb2e6ec4
Dan Vittegleo <dan@vittegleo.com>
2019-11-18 19:03:29 -0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -27,17 +27,3 @@ LOCAL_MODULE_TARGET_ARCH := arm64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/TrichromeWebView.apk
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := chromiumchrome
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
-LOCAL_REQUIRED_MODULES := \
-        libwebviewchromium_loader \
-        libwebviewchromium_plat_support
-
-LOCAL_MODULE_TARGET_ARCH := arm64
-my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/TrichromeChrome.apk
-include $(BUILD_PREBUILT)
