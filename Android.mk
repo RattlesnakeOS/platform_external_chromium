5b73a231ebaf98c3c3f8ddcc8920c89e57a44704
Dan Vittegleo <dan@vittegleo.com>
2019-11-18 20:53:33 -0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,8 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := chromiumlib
+
+LOCAL_MODULE := webview
 LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
 LOCAL_MULTILIB := both
 LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
 LOCAL_REQUIRED_MODULES := \
@@ -11,19 +13,8 @@ LOCAL_REQUIRED_MODULES := \
 
 LOCAL_MODULE_TARGET_ARCH := arm64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/TrichromeLibrary.apk
-include $(BUILD_PREBUILT)
+LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/SystemWebView.apk
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := chromiumwebview
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MULTILIB := both
-LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
-LOCAL_REQUIRED_MODULES := \
-        libwebviewchromium_loader \
-        libwebviewchromium_plat_support
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
 
-LOCAL_MODULE_TARGET_ARCH := arm64
-my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
-LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/TrichromeWebView.apk
 include $(BUILD_PREBUILT)
